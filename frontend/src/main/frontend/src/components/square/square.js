d3e29cdad8cfcf8b5898af84aed07bbf3493b71c
David Kotlirevsky <david.kotlirevsky@gmail.com>
2019-10-04 17:53:17 -0300

    First approach to adding props and states

--- frontend/src/main/frontend/src/components/square/square.js
+++ frontend/src/main/frontend/src/components/square/square.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
+function Square(props){
 
+    return(
+
+            <button className="square" onClick={props.onClick}>
+                {props.value}
             </button>
-        );
-    }
+
+    );
+
 }
 
 export default Square;
